2c8d59dc618e45ccbaf5e397d1465a13ef834d1e
Massimo Colavita <30273901+Massii@users.noreply.github.com>
2021-07-21 22:54:01 +0000

    -

--- obstaculo.h
+++ obstaculo.h
@@ -14,12 +14,10 @@ typedef struct obstaculo {
 } obstaculo_t;
 
 
-obstaculo_t *crear_obstaculo(geometria_t geometria, movimiento_t movimiento, color_t color, poligono_t *poligono);
+obstaculo_t *crear_obstaculo(geometria_t geometria; movimiento_t movimiento; color_t color, poligono_t *poligono);
 
-void destruir_obstaculo(obstaculo_t *obstaculo);
-
-bool consultar_impacto(obstaculo_t *obstaculo);
+void destruir_obstaculo(obstaculo_t obstaculo);
 
 obstaculo_t *obstaculo_crear_desde_archivo(FILE *f);
 
-
+bool consultar_impacto(obstaculo_t);
